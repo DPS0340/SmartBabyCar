d9cfff4b336060e23b887d6a4a2bd29166c9543b
jiho lee <optional.int@kakao.com>
2021-02-23 17:23:48 +0900

    Update SmartBabyCar.ino

--- SmartBabyCar.ino
+++ SmartBabyCar.ino
@@ -1,39 +1,36 @@
 #include <Servo.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 int servoPins[] = {7, 8};
 Servo servos[2];
-const int SERVOS_LENGTH = 1;
-int startAngle = 0;
-Adafruit_MPU6050 mpu;
-sensors_event_t a, g, temp;
-float speedX = 0.0f, speedY = 0.0f; // 수평 보드, 수직 칩에서는 센서 가속도 X가 위아래 방향이므로 제외
-// 엘리베이터 같은 상황 고려하여 위아래 X는 사용하지 않음
-float limit_speed = 15.0f;
+const int SERVOS_LENGTH = 2;
+int sensors[2] = {2, 3};
+const int SENSORS_LENGTH = 1;
+const int limit_rpm = 400;
+const int strange_rpm = limit_rpm + 200;
+const int switchPin = 10;
+int count = 0;
 
 void init_servos();
 void set_servos_angle(int angle);
-void integral_things();
-bool is_over_speed();
+int getRPM(int pin);
+void unlock_wheels();
+void lock_wheels();
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  for(int i=0;i<SENSORS_LENGTH;i++) {
+      pinMode(sensors[i], INPUT_PULLUP);
+      attachInterrupt(digitalPinToInterrupt(sensors[i]), blink, RISING);
+  }
+  pinMode(switchPin, INPUT_PULLUP);
   while (!Serial) {
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
   }
+  Serial.println("initialized!");
   init_servos();
-  unlock_wheels();
-
-  Serial.print("Finding MPU6050..");
-
-  // Try to initialize
-  if (!mpu.begin()) {
-    Serial.println("Failed..");
-  }
-  Serial.println("Found!");
+  lock_wheels();
+  delay(500);
 }
 
 void init_servos() {
@@ -48,41 +45,26 @@ void set_servos_angle(int angle) {
 }
 
 void unlock_wheels() {
-  set_servos_angle(90);
-}
-
-void lock_wheels() {
   set_servos_angle(0);
 }
 
+void lock_wheels() {
+  set_servos_angle(45);
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  integral_acceleration();
-  if(is_over_speed()) {
-    lock_wheels();
-  } else {
+  int rpm = count/2;
+  int switchValue = digitalRead(switchPin);
+  Serial.println(rpm);
+  if(switchValue == LOW) {
     unlock_wheels();
+  } else if(rpm >= limit_rpm) {
+    lock_wheels();
   }
+  count = 0;
+  delay(1000);
 }
 
-void integral_acceleration() {
-  mpu.getEvent(&a, &g, &temp);
-  speedX += a.acceleration.y;
-  speedY += a.acceleration.z;
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-  
-  Serial.print("Speed X: ");
-  Serial.print(speedX);
-  Serial.print(", Y: ");
-  Serial.print(speedY);
-}
-
-bool is_over_speed() {
-  return speedX + speedY >= limit_speed;
+void blink() {
+  count++;
 }
